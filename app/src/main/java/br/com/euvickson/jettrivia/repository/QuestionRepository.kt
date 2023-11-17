994882feeb3ac0b270517e8dc411efdfb3dc37e8
Euvickson <victor.eugennio@gmail.com>
2023-11-17 13:26:03 -0300

    - Finished the QuestionRepository

--- app/src/main/java/br/com/euvickson/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/br/com/euvickson/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package br.com.euvickson.jettrivia.repository
 
+import android.util.Log
 import br.com.euvickson.jettrivia.data.DataOrException
 import br.com.euvickson.jettrivia.model.QuestionItem
 import br.com.euvickson.jettrivia.network.QuestionAPI
@@ -7,5 +8,25 @@ import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionAPI) {
 
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException =
+        DataOrException<ArrayList<QuestionItem>,
+                Boolean,
+                Exception>()
+
+    suspend fun getAllQuestions() : DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        } catch (exception: Exception) {
+
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+
+        }
+        return dataOrException
+    }
 }
